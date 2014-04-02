2e4507693aed57297c8dccb3002c051cdc0e837d
luo xiaobin <ruxsfi@gmail.com>
2014-04-02 13:44:01 +0800

    change kill script to kill openRTSP session only

--- kill_test_client.sh
+++ kill_test_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tokill=`ps -ef|grep 'sh -c.*[o]penRTSP'|awk '{printf $2" "}'`
+tokill=`ps -ef|grep '[o]penRTSP'|grep -v 'sh -c'|awk '{printf $2" "}'`
 sessions_count=`echo $tokill|wc -w`
 if echo $tokill | egrep -q '[0-9]'; then 
   kill -9 $tokill
