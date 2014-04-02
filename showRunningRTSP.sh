a15aa0ff5b10562eccc4891fd8848dd2202f68cd
luo xiaobin <ruxsfi@gmail.com>
2014-04-02 14:33:55 +0800

    fix showOpenRTSP script

--- showRunningRTSP.sh
+++ showRunningRTSP.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-ps -ef | grep 'sh -c.*[o]penRTSP' | awk '{print "Living openRTSP session",NR,"->","running with logfile ->",$16}'
+sessions_count=`ps -ef | grep 'sh -c.*[o]penRTSP' | wc -l` 
+if [ $sessions_count == "0" ]; then
+  echo No living openRTSP session
+else
+  ps -ef | grep 'sh -c.*[o]penRTSP' | awk '{print "Living openRTSP session",NR,"->","running with logfile ->",$16}'
+  echo Total running sessions:  $sessions_count
+fi
